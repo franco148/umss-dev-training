328dbc9cbf7a6b5348e0c1678c913ce808b1dade
franco-extreme <franck_ral@hotmail.com>
2019-10-17 21:26:49 -0400

    springboot-basics: Estructura inicial del proyecto una vez inicializado con spring initialzr.

---
+++ omservice/src/main/java/com/efteam/omservice/OmserviceApplication.java
@@ -0,0 +1,13 @@
+package com.efteam.omservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmserviceApplication.class, args);
+	}
+
+}
