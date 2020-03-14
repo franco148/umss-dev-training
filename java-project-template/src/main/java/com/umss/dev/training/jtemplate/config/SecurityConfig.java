a50b09ffe98a3350c0c71bdd3e29305ef7c587da
Franco Arratia <franck_ral@hotmail.com>
2020-03-13 21:00:18 -0400

    java-project-template-security: Implemented first version of authentication

--- java-project-template/src/main/java/com/umss/dev/training/jtemplate/config/SecurityConfig.java
+++ java-project-template/src/main/java/com/umss/dev/training/jtemplate/config/SecurityConfig.java
@@ -1,4 +1,36 @@
 package com.umss.dev.training.jtemplate.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // 1. FIRST VERSION
+        auth.inMemoryAuthentication()
+                .withUser("franco").password("{noop}password").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}password").roles("USER", "ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // 1. FIRST VERSION
+//        http.authorizeRequests().antMatchers("/", "/h2-console/**").permitAll()
+//                .anyRequest().authenticated();
+//        http.csrf().disable();
+//        http.headers().frameOptions().disable();
+
+        // 2. SECOND VERSION
+        http.authorizeRequests().antMatchers("/", "/h2-console/**").permitAll()
+                .anyRequest().authenticated();
+    }
 }
