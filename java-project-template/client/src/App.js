0a4e79f87ea34cbdc7d512f43ee16fe01e1cf711
Franco Arratia <franck_ral@hotmail.com>
2020-05-03 22:02:40 -0400

    include-client-side-project: Calling a server endpoint from the client app.

--- java-project-template/client/src/App.js
+++ java-project-template/client/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    state = {};
+
+    componentDidMount() {
+        setInterval(this.hello, 250);
+    }
+
+    hello = () => {
+        fetch('/api/users/test-react')
+            .then(response => response.text())
+            .then(message => {
+                this.setState({message: message});
+            });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">{this.state.message}</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
